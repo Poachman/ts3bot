741235fa890cb1543f6cf55a2cf7fa5180fe7132
Poach <chrispoach@gmail.com>
2015-02-22 20:13:13 -0800

    Added looping & begining of idleMover

--- bot.class.php
+++ bot.class.php
@@ -3,6 +3,7 @@
     class tsbot {
         private $config;
         private $server;
+        private $clients;
 
         public function tsbot($configFile = "./config.json") {
             if(file_exists($configFile)) {
@@ -24,6 +25,95 @@
         private function connect() {
             $this->server = TeamSpeak3::factory("serverquery://{$this->config['user']}:{$this->config['pass']}@{$this->config['address']}:{$this->config['port']}/?server_port={$this->config['sport']}");
             // https://docs.planetteamspeak.com/ts3/php/framework/class_team_speak3___node___server.html
+        }
+
+    	public function stop() {
+    		if(file_exists($this->config['brain']))
+    			rename($this->config['brain'], $this->config['brain'] . ".dead");
+    	}
+
+        public function start() {
+    		if(file_exists($this->config['brain']) === false) {
+    			if($this->server !== NULL) {
+    				$this->init();
+    				if(file_put_contents($this->config['brain'], "") === false) {
+    					$this->log("Unable to write " . $this->config['brain']);
+    					return false;
+    				}
+    				while(file_exists($this->config['brain'])) {
+    					$this->tick();
+    					sleep($this->config['loopTime']);
+    				}
+                    $this->deinit();
+    			}
+    			exit();
+    		} else {
+    			$this->log("Bot Already Online", 2);
+    			exit("Bot Already Online\n");
+    			return false;
+    		}
+    	}
+
+        private function init() {
+            // TODO - Change bot name & move bot to another cahnnel
+//    		$this->cmd("clientupdate client_nickname=" . $this->escape($this->config['botNickName']));
+//    		$this->cmd("clientmove clid=" . $this->whoami[0]['client_id'] . " cid=" . $this->config['botCh']);
+
+    		$this->getNewServerInfo();
+        }
+
+        private function deinit() {
+
+        }
+
+        private function tick() {
+
+//            $this->log(var_export($this->server->channelList(), true));
+
+    		$brain = file($this->config['brain']);
+    /*		foreach($brain as $command) {
+    			$this->brainCommand($command);
+    		}*/
+    		$this->getNewServerInfo();
+    		$this->idleMove();
+/*    		$this->functionRooms();
+    		$this->setNormalUsers();
+    		$this->createList();
+            */
+        }
+
+        private function getNewServerInfo() {
+            $this->clients = $this->server->clientList();
+        }
+
+        private function isIdle($client) {
+            if($client->getProperty("client_type") === 0) {
+                    $idletime = $this->config['idletime']['normal'];
+
+                    if($client['client_away'])
+                        $idletime = $this->config['idletime']['away'];
+
+                    if($client['client_input_muted'])
+                        $idletime = $this->config['idletime']['muted'];
+
+                    if($client['client_output_muted'])
+                        $idletime = $this->config['idletime']['deafened'];
+
+                    if($client['client_channel_group_id'] == $this->config['channelAdminGroupId'])
+                        $idletime = $this->config['idletime']['admin'];
+
+                    return $idletime < $client->getProperty("client_idle_time");
+            } else {
+                return false;
+            }
+        }
+
+        private function idleMove() {
+            foreach($this->clients as $client) {
+                if($this->isIdle($client)) {
+                    $client->move($this->config['idleCh']);
+                }
+            }
         }
 
         private function log($string, $level = 1, $cmd = false) {
--- bot.php
+++ bot.php
@@ -3,4 +3,6 @@ require_once "bot.class.php";
 
 $bot = new tsbot();
 
+$bot->start();
+
 ?>

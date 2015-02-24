f83a5111772768420119956fb7961b39741ecca4
Poach <chrispoach@gmail.com>
2015-02-23 21:49:59 -0800

    Spaces to tabs

--- bot.class.php
+++ bot.class.php
@@ -1,170 +1,170 @@
 <?php
-    require_once("./libraries/TeamSpeak3/TeamSpeak3.php");
-    require_once("./functionRoom.class.php");
-    class tsbot {
-        public $config;
-        private $server;
-        private $clients;
-        private $lcid = array();
-        private $functionRooms = array();
+	require_once("./libraries/TeamSpeak3/TeamSpeak3.php");
+	require_once("./functionRoom.class.php");
+	class tsbot {
+		public $config;
+		private $server;
+		private $clients;
+		private $lcid = array();
+		private $functionRooms = array();
 
-        public function tsbot($configFile = "./config.json") {
-            if(file_exists($configFile)) {
-                if(is_readable($configFile)) {
-                    $this->config = json_decode(file_get_contents($configFile), true);
-                    if($this->config === NULL) {
-                        die("Config file invalid");
-                    }
-                } else {
-                    die("Config file can't be read");
-                }
-            } else {
-                die("Config file doesn't exist");
-            }
+		public function tsbot($configFile = "./config.json") {
+			if(file_exists($configFile)) {
+				if(is_readable($configFile)) {
+					$this->config = json_decode(file_get_contents($configFile), true);
+					if($this->config === NULL) {
+						die("Config file invalid");
+					}
+				} else {
+					die("Config file can't be read");
+				}
+			} else {
+				die("Config file doesn't exist");
+			}
 
-            $this->connect();
-        }
+			$this->connect();
+		}
 
-        private function connect() {
-            $this->server = TeamSpeak3::factory("serverquery://{$this->config['user']}:{$this->config['pass']}@{$this->config['address']}:{$this->config['port']}/?server_port={$this->config['sport']}");
-            // https://docs.planetteamspeak.com/ts3/php/framework/class_team_speak3___node___server.html
-        }
+		private function connect() {
+			$this->server = TeamSpeak3::factory("serverquery://{$this->config['user']}:{$this->config['pass']}@{$this->config['address']}:{$this->config['port']}/?server_port={$this->config['sport']}");
+			// https://docs.planetteamspeak.com/ts3/php/framework/class_team_speak3___node___server.html
+		}
 
-    	public function stop() {
-    		if(file_exists($this->config['brain']))
-    			rename($this->config['brain'], $this->config['brain'] . ".dead");
-    	}
+		public function stop() {
+			if(file_exists($this->config['brain']))
+				rename($this->config['brain'], $this->config['brain'] . ".dead");
+		}
 
-        public function start() {
-    		if(file_exists($this->config['brain']) === false) {
-    			if($this->server !== NULL) {
-    				$this->init();
-    				if(file_put_contents($this->config['brain'], "") === false) {
-    					$this->log("Unable to write " . $this->config['brain']);
-    					return false;
-    				}
-    				while(file_exists($this->config['brain'])) {
-    					$this->tick();
-    					sleep($this->config['loopTime']);
-    				}
-                    $this->deinit();
-    			}
-    			exit();
-    		} else {
-    			$this->log("Bot Already Online", 2);
-    			exit("Bot Already Online\n");
-    			return false;
-    		}
-    	}
+		public function start() {
+			if(file_exists($this->config['brain']) === false) {
+				if($this->server !== NULL) {
+					$this->init();
+					if(file_put_contents($this->config['brain'], "") === false) {
+						$this->log("Unable to write " . $this->config['brain']);
+						return false;
+					}
+					while(file_exists($this->config['brain'])) {
+						$this->tick();
+						sleep($this->config['loopTime']);
+					}
+					$this->deinit();
+				}
+				exit();
+			} else {
+				$this->log("Bot Already Online", 2);
+				exit("Bot Already Online\n");
+				return false;
+			}
+		}
 
-        private function init() {
-            // TODO - Change bot name & move bot to another cahnnel
-//    		$this->cmd("clientupdate client_nickname=" . $this->escape($this->config['botNickName']));
-//    		$this->cmd("clientmove clid=" . $this->whoami[0]['client_id'] . " cid=" . $this->config['botCh']);
+		private function init() {
+			// TODO - Change bot name & move bot to another cahnnel
+//			$this->cmd("clientupdate client_nickname=" . $this->escape($this->config['botNickName']));
+//			$this->cmd("clientmove clid=" . $this->whoami[0]['client_id'] . " cid=" . $this->config['botCh']);
 
-    		$this->getNewServerInfo();
-        }
+			$this->getNewServerInfo();
+		}
 
-        private function deinit() {
+		private function deinit() {
 
-        }
+		}
 
-        private function tick() {
-    		$brain = file($this->config['brain']);
-    /*		foreach($brain as $command) {
-    			$this->brainCommand($command);
-    		}*/
-    		$this->getNewServerInfo();
-    		$this->idleMove();
-    		$this->functionRooms();
-/*    		$this->setNormalUsers();
-    		$this->createList();
-            */
-        }
+		private function tick() {
+			$brain = file($this->config['brain']);
+	/*		foreach($brain as $command) {
+				$this->brainCommand($command);
+			}*/
+			$this->getNewServerInfo();
+			$this->idleCheck();
+			$this->functionRooms();
+/*			$this->setNormalUsers();
+			$this->createList();
+			*/
+		}
 
-        private function getNewServerInfo() {
-            $this->server->clientListReset();
-            $this->server->channelListReset();
-            $this->clients = $this->server->clientList();
-        }
+		private function getNewServerInfo() {
+			$this->server->clientListReset();
+			$this->server->channelListReset();
+			$this->clients = $this->server->clientList();
+		}
 
-        private function isIdle($client) {
-            if($client->getProperty("client_type") === 0) {
-                    $idletime = $this->config['idletime']['normal'];
+		private function isIdle($client) {
+			if($client->getProperty("client_type") === 0) {
+					$idletime = $this->config['idletime']['normal'];
 
-                    if($client['client_away'])
-                        $idletime = $this->config['idletime']['away'];
+					if($client['client_away'])
+						$idletime = $this->config['idletime']['away'];
 
-                    if($client['client_input_muted'])
-                        $idletime = $this->config['idletime']['muted'];
+					if($client['client_input_muted'])
+						$idletime = $this->config['idletime']['muted'];
 
-                    if($client['client_output_muted'])
-                        $idletime = $this->config['idletime']['deafened'];
+					if($client['client_output_muted'])
+						$idletime = $this->config['idletime']['deafened'];
 
-                    if($client['client_channel_group_id'] == $this->config['channelAdminGroupId'])
-                        $idletime = $this->config['idletime']['admin'];
+					if($client['client_channel_group_id'] == $this->config['channelAdminGroupId'])
+						$idletime = $this->config['idletime']['admin'];
 
-                    return $idletime < $client->getProperty("client_idle_time");
-            } else {
-                return false;
-            }
-        }
+					return $idletime < $client->getProperty("client_idle_time");
+			} else {
+				return false;
+			}
+		}
 
-        private function isMuted($client) {
-            return $client->getProperty("client_away")
-                || $client->getProperty("client_input_muted")
-                || $client->getProperty("client_output_muted");
-        }
+		private function isMuted($client) {
+			return $client->getProperty("client_away")
+				|| $client->getProperty("client_input_muted")
+				|| $client->getProperty("client_output_muted");
+		}
 
-        private function idleMove() {
-            foreach($this->clients as $client) {
-                if($client->getProperty("cid") != $this->config['idleCh']) {
-                    if($this->isIdle($client)) {
-                        $this->lcid[$client->getProperty("cldbid")] = $client->getProperty("cid");
-                        $client->move($this->config['idleCh']);
-                    }
-                } else {
-                    if(!$this->isIdle($client) && !$this->isMuted($client)) {
-                        if(array_key_exists($client->getProperty("cldbid"), $this->lcid)) {
-                            $client->move($this->lcid[$client->getProperty("cldbid")]);
-                        } else {
-                            $client->move($this->config['lobbyCh']);
-                        }
-                    }
-                }
-            }
-        }
+		private function idleCheck() {
+			foreach($this->clients as $client) {
+				if($client->getProperty("cid") != $this->config['idleCh']) {
+					if($this->isIdle($client)) {
+						$this->lcid[$client->getProperty("cldbid")] = $client->getProperty("cid");
+						$client->move($this->config['idleCh']);
+					}
+				} else {
+					if(!($this->isIdle($client) || $this->isMuted($client))) {
+						if(array_key_exists($client->getProperty("cldbid"), $this->lcid)) {
+							$client->move($this->lcid[$client->getProperty("cldbid")]);
+						} else {
+							$client->move($this->config['lobbyCh']);
+						}
+					}
+				}
+			}
+		}
 
-        private function functionRooms() {
-            $functionRoom = $this->server->channelList(array("cid" => $this->config['newFnRoomId']))[$this->config['newFnRoomId']];
+		private function functionRooms() {
+			$functionRoom = $this->server->channelList(array("cid" => $this->config['newFnRoomId']))[$this->config['newFnRoomId']];
 
-            foreach($functionRoom->clientList() as $client) {
-                $this->functionRooms[] = new functionRoom($this->server, $client, $this);
-            }
+			foreach($functionRoom->clientList() as $client) {
+				$this->functionRooms[] = new functionRoom($this->server, $client, $this);
+			}
 
-            foreach($this->functionRooms as $functionRoom) {
-                $functionRoom->tick();
-            }
-        }
+			foreach($this->functionRooms as $functionRoom) {
+				$functionRoom->tick();
+			}
+		}
 
-        private function log($string, $level = 1, $cmd = false) {
-            if(!is_string($string)) $string = var_export($string, true);
-            $string = date("m/d/y H:i:s - ") . $string;
-            if($level > 0) {
-                $string .= "\n";
-                echo $string;
-                explode("\n", $string);
-                if(file_exists($this->config['logFile'])) {
-                    $log = file($this->config['logFile']);
-                }
-                $log[] = $string;
-                implode("\n", $log);
-                file_put_contents($this->config['logFile'], $log);
-            }
-            if($level > 5) {
-                die();
-            }
-        }
+		public function log($string, $level = 1, $cmd = false) {
+			if(!is_string($string)) $string = var_export($string, true);
+			$string = date("m/d/y H:i:s - ") . $string;
+			if($level > 0) {
+				$string .= "\n";
+				echo $string;
+				explode("\n", $string);
+				if(file_exists($this->config['logFile'])) {
+					$log = file($this->config['logFile']);
+				}
+				$log[] = $string;
+				implode("\n", $log);
+				file_put_contents($this->config['logFile'], $log);
+			}
+			if($level > 5) {
+				die();
+			}
+		}
 
-    }
+	}
 ?>
--- functionRoom.class.php
+++ functionRoom.class.php
@@ -9,8 +9,9 @@
 			$this->admin = $channelAdmin;
 			$this->init();
 			$this->channel = $server->channelCreate(array(
-				"channel_name"	=> "{$this->admin->getProperty("client_nickname")}'s Room",
-				"cpid"			=> $this->bot->config['fnRoomSpacerId']
+				"channel_name"				=> "{$this->admin->getProperty("client_nickname")}'s Room",
+				"cpid"						=> $this->bot->config['fnRoomSpacerId'],
+				"channel_flag_permanent"	=> "1"
 			));
 			$this->admin->move($this->channel);
 			$this->admin->setChannelGroup($this->channel, $this->bot->config['channelAdminGroupId']);
